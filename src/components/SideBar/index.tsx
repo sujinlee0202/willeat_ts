9b4df9663961a6d227f1a81db14d8fc0dc913557
titch <sujinlee0202@gmail.com>
2023-04-28 03:45:50 +0900

    sidebar close 버튼 추가

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -29,7 +29,8 @@ const SideBar = () => {
   const onClickCloseButton = () => setClose(prev => !prev)
 
   return (
-    <nav className='absolute top-0 left-0 z-10 w-96 bg-white h-full flex flex-col items-center border-r'>
+    <nav className={`absolute top-0 left-0 z-10 w-96 bg-white h-full flex flex-col items-center border-r 
+      transition-all duration-500 ease-in-out ${close && (openPlaceDetail ? '-left-[768px]' : '-left-[384px]')}`}>
       <div className="w-full border-b flex items-center justify-center h-24">
         <Link to='/' className='text-orange-400 font-bold text-4xl p-1' onClick={onClickHome}>WillEat</Link>
       </div>
@@ -40,13 +41,20 @@ const SideBar = () => {
         <>
           <PlaceDetail />
           <button 
-            className="w-10 h-10 absolute border border-l-0 bg-white top-4 left-[768px] flex items-center justify-center rounded-r-lg hover:bg-blue-50"
+            className={`w-10 h-10 absolute border border-l-0 bg-white top-4 left-[768px] flex items-center justify-center rounded-r-lg hover:bg-blue-50 `}
             onClick={onClickCloseDetail}
           >
             <AiOutlineClose />
           </button>
         </>
       )}
+      <button 
+        className={`h-12 absolute top-1/2 ${openPlaceDetail ? 'left-[768px]' : 'left-[384px]'} bg-white hover:bg-blue-50 
+          z-20 border border-l-0 rounded-r-lg px-0.5 py-1 -translate-y-1/2`}
+        onClick={onClickCloseButton}
+      >
+        {close ? <AiOutlineRight className="text-sm" /> : <AiOutlineLeft className="text-sm" />}
+      </button>
     </nav>
   )
 }
